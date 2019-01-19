157fa008484ae9f237516ac41f4af7bac08a07d5
Marianne Tromp <mtromp@mtromp.com>
2019-01-19 18:32:24 -0500

    First test return code check

---
+++ mngdockerdev/spec/buildDockerImage_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec/bash'
+
+BUILD_IMAGE_SCRIPT='./buildDockerImage'
+describe 'TestBuildDockerImage' do
+  include Rspec::Bash
+  let(:stubbed_env) { create_stubbed_env }
+
+  context 'help message is displayed' do
+    it 'exits zero when run' do
+      stdout, stderr, status = stubbed_env.execute(BUILD_IMAGE_SCRIPT)
+      expect(status.exitstatus).to eq 0
+    end
+  end
+
+end
